8748ad25e689589033e911900df3e974cbcf59be
talanthier <talanthier@gmail.com>
2022-09-21 11:54:18 -0700

    standardized column names

--- scraper.py
+++ scraper.py
@@ -1,17 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import pycountry
+
+list_states = ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL',
+               'IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE',
+               'NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD',
+               'TN','TX','UT','VT','VA','WA','WV','WI','WY']
+col_names = {'First' : 'First Name', 'Last' : 'Last Name', 'State or Country':'State/Country'}
 
 
 def get_results(url, outfile):
     '''Scrapes official Western States results and writes results to a csv file.'''
     req = requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
-    
     list_rows = soup.find_all('tr') # finds all table rows
     header = []
     for col in list_rows[0].find_all('th'):
         header.append(col.string) # appends column names
+    header = [col_names.get(x,x) for x in header]
     num_cols = len(header)
     with open(outfile, 'w', encoding = 'utf8', newline = '') as f:
         writer = csv.writer(f)
